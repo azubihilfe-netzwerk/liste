0f19a632dec7dd61770ccf6745a7275d7b2df53a
labpa <pbienenstein@posteo.de>
2025-06-04 18:15:17 +0200

    test console.log

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from 'react';
 
 const Dashboard: FC = () => {
+    console.log("Test")
+
+
     return (
         <div className="min-h-screen flex flex-col items-center justify-start pt-6 ">
             <h1 className="text-4xl font-bold text-navbar-blue">Azubihilfe Netzwerk</h1>
