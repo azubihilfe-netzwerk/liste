fdeb1042c4d221cf2e63c164d8f4908390d0c23f
labpa <pbienenstein@posteo.de>
2025-06-04 18:04:05 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter basename={process.env.NODE_ENV === 'production' ? "/liste" : "/"} >
+        <App />
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
